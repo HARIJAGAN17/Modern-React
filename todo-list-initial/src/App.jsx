50c23b6990b0d6dc462255691d870c3a28c3a7c8
Harijagan.T <harijagan.t@ust.com>
2024-12-04 12:12:10 +0530

    created a context for the app

--- todo-list-initial/src/App.jsx
+++ todo-list-initial/src/App.jsx
@@ -1,21 +1,44 @@
-import Header from './components/Header.jsx';
-import Home from './views/Home.jsx';
+import Header from "./components/Header.jsx";
+import Home from "./views/Home.jsx";
 import "./App.scss";
+import { useState } from "react";
+import TodosContext from "./TodosContext.js";
+
+const initialTodos = [
+  {
+    id: 0,
+    title: "Do Groceries",
+    description: "Buy apples, rice, juice and toilet paper.",
+    isDone: true,
+  },
+  {
+    id: 1,
+    title: "Study React",
+    description: "Understand context & reducers.",
+    isDone: false,
+  },
+  {
+    id: 2,
+    title: "Learn Redux",
+    description: "Learn state management with Redux",
+    isDone: false,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
 
   return (
     <>
       <main>
-        
-        <Header appName="To-Do List with React" />
+        <TodosContext.Provider value ={{todos,setTodos}}>
+          <Header appName="To-Do List with React" />
 
-        <Home />
-
-        
+          <Home />
+        </TodosContext.Provider>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
