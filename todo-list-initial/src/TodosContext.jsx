3790b86e5aca19153f74903d71c1ce8f70cead16
Harijagan.T <harijagan.t@ust.com>
2024-12-07 14:56:06 +0530

    adding todo with id to list-added

--- todo-list-initial/src/TodosContext.jsx
+++ todo-list-initial/src/TodosContext.jsx
@@ -59,6 +59,12 @@ function TodosReducer(todos, action) {
     case "deleted":
       if (confirm("Are you sure you wan to delete ?"))
         return todos.filter((item) => item.id !== action.elId);
+
+    case "add":
+      let newTodo = action.newTodo;
+      newTodo.id =todos.length ? Math.max(...todos.map((item)=>item.id))+1:0;
+      return [...todos,newTodo]
+
     case "toggle":
       return todos.map((item) => {
         if (item.id === action.elId) {
--- todo-list-initial/src/components/modals/AddTodoModal.jsx
+++ todo-list-initial/src/components/modals/AddTodoModal.jsx
@@ -1,19 +1,59 @@
+import { useState } from "react";
+import { useTodos } from "../../TodosContext";
+
 function AddTodoModal() {
+  const store = useTodos();
+
+  
+ const [newTodo,setNewTodo] = useState({
+  title:"",
+  description:"",
+  isDone:false,
+ });
+
+ function handleChange(e){
+  const {name,value} = e.target;
+
+  setNewTodo((prev)=>({
+    ...prev,
+    [name]:value
+  }));
+ }
+
+
+  function addHandler() {
+    if (newTodo.title && newTodo.description) {
+      store.dispatch({ type: "add", newTodo });
+      store.setModals(false);
+
+    } else {
+      alert("Please fill both the values properly");
+    }
+  }
+
   return (
     <>
       <div className="form">
         <h3>Add a new task</h3>
         <label htmlFor="title">Title *</label>
-        <input type="text" name="title" placeholder="Enter a title..." />
+        <input type="text" name="title" placeholder="Enter a title..." value={newTodo.title} onChange={handleChange} />
         <br />
         <label htmlFor="description">Description *</label>
         <textarea
           name="description"
           rows="4"
           placeholder="Enter a description..."
+          value={newTodo.description} onChange={handleChange}
         />
         <br />
-        <button className="btn gray">Add Task</button>
+        <button
+          onClick={() => {
+            addHandler();
+          }}
+          className="btn gray"
+        >
+          Add Task
+        </button>
       </div>
     </>
   );
