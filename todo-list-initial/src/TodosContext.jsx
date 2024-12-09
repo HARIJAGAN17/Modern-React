79b63aaa4aaea06aeb7a9f2d2be016a9d901c442
Harijagan.T <harijagan.t@ust.com>
2024-12-09 22:08:02 +0530

    ui update

--- todo-list-initial/src/TodosContext.jsx
+++ todo-list-initial/src/TodosContext.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export const TodosContext = createContext("");
 
+
 const initialTodos = localStorage.getItem("todos").length
   ? JSON.parse(localStorage.getItem("todos"))
   : [];
