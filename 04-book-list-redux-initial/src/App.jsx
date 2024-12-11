a64efcdecc60eb5ca0e725c029333a4deb1c0596
Harijagan.T <harijagan.t@ust.com>
2024-12-11 07:21:39 +0530

    checking with route params

--- 04-book-list-redux-initial/src/App.jsx
+++ 04-book-list-redux-initial/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<BooksPage />} />
           <Route path="add-book" element={<AddBookPage />} />
-          <Route path="book" element={<SingleBookPage />} />
+          <Route path="book/:id" element={<SingleBookPage />} />
         </Routes>
       </BrowserRouter>
     </>
