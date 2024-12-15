a72ef9416cc3c09f8bed0f3f4774d70fd8657e76
Harijagan.T <harijagan.t@ust.com>
2024-12-15 11:24:48 +0530

    added 404 page and improved ui

--- 04-book-list-redux-initial/src/views/ErrorPage.jsx
+++ 04-book-list-redux-initial/src/views/ErrorPage.jsx
@@ -1,9 +1,25 @@
-function ErrorPage() {
-  return (
-    <>
-      <h1>Page not found</h1>
-    </>
-  );
-}
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './NotFound.css'; // Import the CSS file for styling
+
+const ErrorPage = () => {
+    const navigate = useNavigate();
+
+    const handleGoHome = () => {
+        navigate('/'); // Redirect to the homepage
+    };
+
+    return (
+        <div className="not-found-container">
+            <div className="not-found-content">
+                <h1 className="not-found-title">404</h1>
+                <p className="not-found-message">Oops! The page you are looking for does not exist.</p>
+                <button className="not-found-button" onClick={handleGoHome}>
+                    Go to Homepage
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default ErrorPage;
