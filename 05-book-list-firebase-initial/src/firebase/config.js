7e771bf42fe9a96ebc802373b64c54ca33845f2c
Harijagan.T <harijagan.t@ust.com>
2024-12-17 08:19:29 +0530

    setup firebase -updated

---
+++ 05-book-list-firebase-initial/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYP_NUEr_Yh8YF1_Ge2q71rWFdEAfywbA",
+  authDomain: "book-list-with-fb.firebaseapp.com",
+  projectId: "book-list-with-fb",
+  storageBucket: "book-list-with-fb.firebasestorage.app",
+  messagingSenderId: "120521283820",
+  appId: "1:120521283820:web:939c1ccbaa2ec87a6c2dc9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
