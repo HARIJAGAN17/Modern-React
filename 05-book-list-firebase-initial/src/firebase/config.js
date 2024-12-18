1e752df9652d4c2c5519648b7c4cea88dcc9e24e
Harijagan.T <harijagan.t@ust.com>
2024-12-18 19:49:51 +0530

    check auth

--- 05-book-list-firebase-initial/src/firebase/config.js
+++ 05-book-list-firebase-initial/src/firebase/config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBYP_NUEr_Yh8YF1_Ge2q71rWFdEAfywbA",
   authDomain: "book-list-with-fb.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:120521283820:web:939c1ccbaa2ec87a6c2dc9",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
