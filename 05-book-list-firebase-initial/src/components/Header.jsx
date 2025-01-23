046ecc847daa639fd6af2a60aa2b2b893b5146a2
harijagan <jaganhari2002@gmail.com>
2025-01-23 13:07:54 +0530

    added logout functionality

--- 05-book-list-firebase-initial/src/components/Header.jsx
+++ 05-book-list-firebase-initial/src/components/Header.jsx
@@ -1,36 +1,40 @@
 import { NavLink } from "react-router-dom";
+import { auth } from "../firebase/config";
+import { signOut } from "firebase/auth";
 
-function Header({pageTitle}) {
-
-    return (
-      <>
-
-            <h1>{pageTitle}</h1>
-
-            <div className="header-btns">
-
-                    <NavLink to="/">
-                      <button className="btn">
-                          Books
-                      </button>
-                    </NavLink>
-
-                    <NavLink to="/add-book">
-                      <button className="btn">
-                          Add Book +
-                      </button>
-                    </NavLink>
-
-                    <button className="btn transparent">
-                      Logout
-                    </button>
-
-               
-            </div>
-    
-      </>
-    )
+function Header({ pageTitle }) {
+  function handleSignOut() {
+    if (confirm("Are you sure you want to logout?")) {
+      signOut(auth)
+        .then(() => {})
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
-  
-  export default Header
-  
+  return (
+    <>
+      <h1>{pageTitle}</h1>
+
+      <div className="header-btns">
+        <NavLink to="/">
+          <button className="btn">Books</button>
+        </NavLink>
+
+        <NavLink to="/add-book">
+          <button className="btn">Add Book +</button>
+        </NavLink>
+
+        <button
+          onClick={() => {
+            handleSignOut();
+          }}
+          className="btn transparent"
+        >
+          Logout
+        </button>
+      </div>
+    </>
+  );
+}
+export default Header;
