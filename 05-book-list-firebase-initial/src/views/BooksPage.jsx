7c657b2538e9d2313c822945a677db67f2800c76
harijagan <jaganhari2002@gmail.com>
2025-01-27 12:28:10 +0530

    fetch books of particular logged user

--- 05-book-list-firebase-initial/src/views/BooksPage.jsx
+++ 05-book-list-firebase-initial/src/views/BooksPage.jsx
@@ -1,16 +1,19 @@
 import Book from "../components/Book.jsx";
 import Header from "../components/Header.jsx";
 import { useEffect, useState } from "react";
-import { collection, query, getDocs } from "firebase/firestore";
+import { collection, query, getDocs, where } from "firebase/firestore";
 import { db } from "../firebase/config.js";
+import { useSelector } from "react-redux";
+import { selectUsers } from "../store/userSlice.js";
 
 function BooksPage() {
+  const uid = useSelector(selectUsers).currentUser.id;
   const [books, setBooks] = useState([]);
   const pageTitle = "📖 Book List with Router,Firebase";
 
   useEffect(() => {
     const fetchBooks = async () => {
-      const q = query(collection(db, "books"));
+      const q = query(collection(db, "books"), where("user_id", "==", uid));
 
       const querySnapshot = await getDocs(q);
       let bookList = [];
